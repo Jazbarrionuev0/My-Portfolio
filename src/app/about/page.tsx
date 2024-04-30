3de32770bd8797c5eb11725daa2c8dc047d2df54
Jazmín <jazminbarrionuevo23@gmail.com>
2024-04-29 21:51:22 -0300

    tilde

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,9 +7,9 @@ export default async function WorkPage() {
       <div className="flex flex-wrap justify-center gap-10 md:justify-between">
         <Image className="w-72 rounded-sm m-5" src={"/travel.jpg"} alt="Jazmin Barrionuevo" width={564} height={420} />
         <p className="mt-4 md:mt-6 lg:mt-8 md:text-base text-base lg:text-lg">
-          Hi! I'm Jazmin, and I'm a software developer. And yes, I do other things apart from coding, such as playing
-          the games that I've coded, writing about what I've learned from coding, and sometimes even let myself go a
-          little bit crazy a see Mark Zuckerberg's movie.
+          Hi! I&apos;m Jazmin, and I&apos;m a software developer. And yes, I do other things apart from coding, such as
+          playing the games that I&apos;ve coded, writing about what I&apos;ve learned from coding, and sometimes even
+          let myself go a little bit crazy a see Mark Zuckerberg&apos;s movie.
         </p>
       </div>
     </div>
--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -3,7 +3,7 @@ export default async function AboutMe() {
     <section className="flex items-center justify-center flex-col md:flex-row  min-h-80">
       <div className="px-4 md:pr-8 flex flex-col gap-3">
         <h4 className="text-sm md:text-lg text-[#FF00CC] font-semibold ">HELLO, MY NAME IS JAZMIN</h4>
-        <h1 className="text-5xl md:text-7xl">I'm a software developer.</h1>
+        <h1 className="text-5xl md:text-7xl">I&apos;m a software developer.</h1>
         <p className="text-2xl md:text-3xl">
           I’m a software developer and co-founder of{" "}
           <span className="slid cursor-pointer underline decoration-[#FF00CC] decoration-8 "> NeoPixel Software </span>{" "}
