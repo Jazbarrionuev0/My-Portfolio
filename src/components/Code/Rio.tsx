f7980a4c0d7fb3ab660851ddccb71dcafa076f11
Jazmín <jazminbarrionuevo23@gmail.com>
2024-08-08 23:33:10 -0300

    about me

--- src/components/Code/Rio.tsx
+++ src/components/Code/Rio.tsx
@@ -1,13 +1,14 @@
-"use client"
-import { CodeBlock, dracula } from 'react-code-blocks';
+"use client";
+import { CodeBlock, dracula } from "react-code-blocks";
 
-export default function Rio({ code, language, showLineNumbers }: { code: string, language: string, showLineNumbers: boolean }) {
-    return (
-        <CodeBlock
-            text={code}
-            language={language}
-            showLineNumbers={showLineNumbers}
-            theme={dracula}
-        />
-    );
+export default function Rio({
+  code,
+  language,
+  showLineNumbers,
+}: {
+  code: string;
+  language: string;
+  showLineNumbers: boolean;
+}) {
+  return <CodeBlock text={code} language={language} showLineNumbers={showLineNumbers} theme={dracula} />;
 }
