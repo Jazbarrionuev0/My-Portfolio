9a1944ef3f53a89262053a5fc97b738b255c8338
LucaPalminteri <lucapalminteri02@gmail.com>
2025-08-01 15:01:50 -0300

    add portfolio domain

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "www.jazminbarrionuevo.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
   experimental: {
